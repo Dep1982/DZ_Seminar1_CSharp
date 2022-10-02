569a885a0b32d257f0e9cf81ff37a867b8940a97
Dmitry <dep1982@mail.ru>
2022-10-02 21:10:41 +0300

    Задача на большее число

---
+++ DZ1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите два числа");
+/*
+string number1 = Console.ReadLine();
+string number2 = Console.ReadLine();
+var Number1 = int.Parse(number1);
+var Number2 = int.Parse(number2); 
+*/
+int Number1 = int.Parse(Console.ReadLine());
+int Number2 = int.Parse(Console.ReadLine());
+if (Number1 > Number2)
+{
+    Console.WriteLine("Первое число больше");
+}
+else if (Number1 < Number2)
+{
+    Console.WriteLine("Второе число больше");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
