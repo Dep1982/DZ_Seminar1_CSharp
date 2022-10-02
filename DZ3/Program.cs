feb4b24b7af3d0dbf2da6413fa26d234df166e1c
Dmitry <dep1982@mail.ru>
2022-10-03 00:51:40 +0300

    Задача на четное и нечетное числа

---
+++ DZ3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+/*
+string number1 = Console.ReadLine();
+var Number1 = int.Parse(number1);
+*/
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine($"{number} четное число");
+}
+else
+{
+    Console.WriteLine($"{number} не четное число");
+}
